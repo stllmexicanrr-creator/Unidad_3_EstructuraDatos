e581040e816e23ae158ca2d58ce8c16028bca5f5
(M.Stll) <stllmexicanrr@gmail.com>
2025-10-10 09:29:28 -0600

    Unidad III Avance 10 de octubre

---
+++ src/unidad_3/Unidad_3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unidad_3;
+
+/**
+ *
+ * @author 1175126399-32299  // Regla 3 - documentar todas las clases, de quien lo hizo, cuando lo hizo y por que lo hizo
+ */
+public class Unidad_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
